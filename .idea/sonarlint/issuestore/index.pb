f253efc123654cc89f003cc59fc853a6e00a9ac0
ralph.panitz <ralph.panitz@mesoneer.io>
2022-10-25 11:52:13 +0200

    initial

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+k
+;src/main/java/io/flowsquad/camunda/test/MailingService.java,8/2/828bfc01e7c92ba176c17c5348ffd185c5fdb602
+m
+=src/main/java/io/flowsquad/camunda/test/RollbackScenario.java,c/d/cd13e85dffb23012b6453db358ed3f7674f4e2aa
+h
+8src/main/java/io/flowsquad/camunda/test/Application.java,d/9/d92bd6de437d0c744e222e2d0f419bf847ea4311
+i
+9src/test/java/io/flowsquad/camunda/test/WorkflowTest.java,2/9/29de6397f5e67e643d6a4fb3de553a495a96eb32
+p
+@src/test/java/io/flowsquad/camunda/test/DeliveryProcessTest.java,e/5/e5da74b59b62649db05ab86ea29e18e69abe2072
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
---
+++ src/test/java/io/flowsquad/camunda/test/DeliveryProcessTest.java
@@ -0,0 +1,89 @@
+package io.flowsquad.camunda.test;
+
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRule;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
+import org.camunda.bpm.scenario.ProcessScenario;
+import org.camunda.bpm.scenario.Scenario;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.taskService;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.withVariables;
+import static org.mockito.Mockito.*;
+
+@Deployment(resources = "bpmn/deliver-process.bpmn")
+public class DeliveryProcessTest {
+
+    public static final String PROCESS_KEY = "deliveryprocess";
+    public static final String TASK_DELIVER_ORDER = "Task_DeliverOrder";
+    public static final String VAR_ORDER_DELIVERED = "orderDelivered";
+    public static final String END_EVENT_DELIVERY_COMPLETED = "EndEvent_DeliveryCompleted";
+    public static final String END_EVENT_DELIVERY_CANCELLED = "EndEvent_DeliveryCancelled";
+
+    @Rule
+    @ClassRule
+    public static TestCoverageProcessEngineRule rule = TestCoverageProcessEngineRuleBuilder.create()
+            .assertClassCoverageAtLeast(0.9)
+            .build();
+
+    @Mock
+    private ProcessScenario testDeliveryProcess;
+
+    @Before
+    public void defaultScenario() {
+        MockitoAnnotations.initMocks(this);
+
+        //Happy-Path
+        when(testDeliveryProcess.waitsAtUserTask(TASK_DELIVER_ORDER))
+                .thenReturn(task -> {
+                    task.complete(withVariables(VAR_ORDER_DELIVERED, true));
+                });
+    }
+
+    @Test
+    public void shouldExecuteHappyPath() {
+        Scenario.run(testDeliveryProcess)
+                .startByKey(PROCESS_KEY)
+                .execute();
+
+        verify(testDeliveryProcess)
+                .hasFinished(END_EVENT_DELIVERY_COMPLETED);
+    }
+
+    @Test
+    public void shouldExecuteOrderCancelled() {
+        when(testDeliveryProcess.waitsAtUserTask(TASK_DELIVER_ORDER)).thenReturn(task -> {
+            taskService().handleBpmnError(task.getId(), "DeliveryCancelled");
+        });
+
+        Scenario.run(testDeliveryProcess)
+                .startByKey(PROCESS_KEY)
+                .execute();
+
+        verify(testDeliveryProcess)
+                .hasFinished(END_EVENT_DELIVERY_CANCELLED);
+    }
+
+    @Test
+    public void shouldExecuteDeliverTwice() {
+        when(testDeliveryProcess.waitsAtUserTask(TASK_DELIVER_ORDER)).thenReturn(task -> {
+            task.complete(withVariables(VAR_ORDER_DELIVERED, false));
+        }, task -> {
+            task.complete(withVariables(VAR_ORDER_DELIVERED, true));
+        });
+
+        Scenario.run(testDeliveryProcess)
+                .startByKey(PROCESS_KEY)
+                .execute();
+
+        verify(testDeliveryProcess, times(2))
+                .hasCompleted(TASK_DELIVER_ORDER);
+        verify(testDeliveryProcess)
+                .hasFinished(END_EVENT_DELIVERY_COMPLETED);
+    }
+}
